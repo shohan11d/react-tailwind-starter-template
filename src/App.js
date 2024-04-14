964d18ab68226d6d362e6afb1abb747f58fc609b
shohan11d <shohan11d@gmail.com>
2024-04-14 10:08:22 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <p className="underline font-bold text-center text-2xl mt-5">
+        Lorem ipsum dolor sit amet.
+      </p>
+    </div>
+  );
+}
+
+export default App;
